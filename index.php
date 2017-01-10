94ba1464c3c77d71e635276f6c584ed13ac85825
thi90 <thierry.galabru@yahoo.fr>
2017-01-10 11:43:21 +0100

    changement de H2

--- index.php
+++ index.php
@@ -4,6 +4,18 @@
 	<title>Mon site avec github	</title>
 </head>
 <body>
+<header>	
+  <h1>Mon site avec Github	</h1> 
+
+</header>
+<main>	
+<section>	
+       <h2>	MA PREMIERE SECTION</h2>
+</section>
+<footer>	
+tous droits reserves
+</footer>
+ </main>
 
 </body>
 </html>
